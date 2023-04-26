a8ff691b68fa1e9246c51b380525303e454ee04d
satish rajbhar <satissraj@gmail.com>
2023-04-27 02:43:50 +0530

    fresh restart

--- app/index.ts
+++ app/index.ts
@@ -1,32 +1,21 @@
 import { ApolloServer } from "apollo-server-express";
-// import Schema from "./Schema";
-// import Resolvers from "./Resolvers";
 import express from "express";
 import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
 import http from "http";
 import { schema, dataSourcesAPIs } from './sources';
-import { startStandaloneServer } from '@apollo/server/standalone';
+const port = process.env.PORT || 4000;
 (async()=>{
   const app = express();
   const httpServer = http.createServer(app);
   const server = new ApolloServer({
     schema,
-    // resolvers,
     dataSources: ():any =>dataSourcesAPIs(undefined),
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   }) as any;
-  // const { url } = await startStandaloneServer(server, {
-  //   context: async (context) => {
-  //     return {
-  //       dataSources: dataSourcesAPIs(context),
-  //     };
-  //   },
-  // });
-  // console.log("🚀 ~ file: index.ts:25 ~ url:", url)
   await server.start();
   server.applyMiddleware({ app });
   await new Promise<void>((resolve) =>
-    httpServer.listen({ port: 4000 }, resolve)
+    httpServer.listen({ port }, resolve)
   );
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
 })()
