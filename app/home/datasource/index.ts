5d3ff1dbe58880e91fc8232c8f13e6fb7c309a11
satish rajbhar <satissraj@gmail.com>
2023-04-30 04:07:23 +0530

    realtime hoc added

--- app/home/datasource/index.ts
+++ app/home/datasource/index.ts
@@ -2,6 +2,17 @@ import { RESTDataSource } from "@apollo/datasource-rest";
 
 class HomeDataSource extends RESTDataSource{
   async getData() {
+    await this.get("https://reqres.in/api/unknown")
+    await this.get("https://reqres.in/api/unknown")
+    await this.get("https://reqres.in/api/unknown")
+    await this.get("https://reqres.in/api/unknown")
+    await this.get("https://reqres.in/api/unknown")
+    await this.get("https://reqres.in/api/unknown")
+    await this.get("https://reqres.in/api/unknown")
+    await this.get("https://reqres.in/api/unknown")
+    await this.get("https://reqres.in/api/unknown")
+    await this.get("https://reqres.in/api/unknown")
+    await this.get("https://reqres.in/api/unknown")
     return "124"
   }
 }
--- app/home/index.ts
+++ app/home/index.ts
@@ -4,13 +4,44 @@ import * as Demo from '../layout/demo.json'
 const typeDefs = gql`
  type  Query {
   getLayout(page:String): JSON
+  getDynamicAttributes(params:JSON!): JSON
+  getDynamicComponents(params:JSON!): JSON
 }
 `
 const resolver = {
   Query: {
     getLayout: async (_:any, __:any, { dataSources }:any) => {
-      // return dataSources.homeAPI.getData();
       return Demo
+    },
+    getDynamicAttributes: async (_: any, { params}:any, { dataSources }:any) => {
+      return {
+        ...params ?? {},
+        style: {
+          ...params?.style ?? {},
+          color:"green"          
+        },
+      }
+    },
+    getDynamicComponents: async (_: any, __: any, { dataSources }: any) => {
+      await dataSources.homeAPI.getData()
+      return [
+        {
+          "type":"Text",
+          "attributes":{
+            "style":{
+            },
+            "children":"item 1"
+          }
+        },
+        {
+          "type":"Text",
+          "attributes":{
+            "style":{
+            },
+            "children":"item 2"
+          }
+        } 
+      ]
     },
   }
 }
